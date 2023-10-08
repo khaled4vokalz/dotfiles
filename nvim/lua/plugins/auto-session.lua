bbab2bfd95ffb302016558a1011f7ff9db397879
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2023-10-08 21:25:30 +0600

    make auto sessions work
    - we need to close the neo-tree buffer before auto session saves a session, else it fails to restore it properly next time
    - also we need to open it when the sessions are restored

--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -1,13 +1,37 @@
+---@diagnostic disable: assign-type-mismatch, missing-fields
 return {
   "rmagatti/auto-session",
   config = function()
-    -- vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-    local auto_session = require("auto-session")
+    local function close_nvim_tree()
+      require("neo-tree.command").execute({
+        -- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/command/init.lua#L23
+        action = "close",
+      })
+    end
+    local function open_nvim_tree()
+      require("neo-tree.command").execute({
+        -- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/command/init.lua#L23
+        action = "show",
+      })
+    end
 
-    auto_session.setup({
+    -- auto-session basically fails for opened neo-tree for sessions_dir
+    -- so we need to close the neo-tree before auto-session saves a session
+    -- and open the neo-tree when it restores a session
+    require("auto-session").setup({
       log_level = error,
+      pre_save_cmds = { close_nvim_tree },
+      post_save_cmds = { open_nvim_tree },
+      post_open_cmds = { open_nvim_tree },
+      post_restore_cmds = { open_nvim_tree },
+      auto_save_enabled = true,
       auto_restore_enabled = true,
       auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      cwd_change_handling = {
+        restore_upcoming_session = true,
+        pre_cwd_changed_hook = close_nvim_tree,
+        post_cwd_changed_hook = open_nvim_tree,
+      },
     })
 
     local keymap = vim.keymap
