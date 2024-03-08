007830e4e22f59d8a1453cbfa9e5f6986658e375
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2024-03-08 18:25:25 +0600

    Lazy sync and tmux quit config change

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,3 +17,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+keymap.set("n", "<leader>dd", function()
+  vim.diagnostic.open_float()
+end, { noremap = true, desc = "Open floating diagnostics window" }) --  move current buffer to new tab
