75f00d7bc597671e3a1a44946bf5f33c4031f237
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2024-05-11 19:57:05 +0600

    Update plugins + add keymap for better searching for references

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -21,3 +21,10 @@ keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer
 keymap.set("n", "<leader>dd", function()
   vim.diagnostic.open_float()
 end, { noremap = true, desc = "Open floating diagnostics window" }) --  move current buffer to new tab
+
+-- don't yank text on pasting over selection
+keymap.set("x", "p", "P", { silent = true })
+
+keymap.set("n", "<leader>far", function()
+  vim.lsp.buf.references()
+end, { noremap = true, desc = "Find all references " })
