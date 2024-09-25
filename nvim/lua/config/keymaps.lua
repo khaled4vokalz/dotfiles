eb1c883955b831e4aafeaf7286ca3844a0b52687
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2024-09-25 09:21:40 +0600

    toggle virtual texts

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -18,6 +18,8 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
+keymap.set("n", "<leader>vt", "<cmd>DiagnosticsToggleVirtualText<CR>", { desc = "Toggle diagnostic virtual texts" })
+
 keymap.set("n", "<leader>dd", function()
   vim.diagnostic.open_float()
 end, { noremap = true, desc = "Open floating diagnostics window" }) --  move current buffer to new tab
