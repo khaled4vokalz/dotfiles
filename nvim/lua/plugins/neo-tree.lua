29a725d4e91cb1497f167f538a4473414f7f20fc
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2024-10-15 13:01:45 +0600

    use neo tree prompts for file name selection using `Y`
    - also added new config so that only specific folders and files are kept hidden not all dotfiles

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,57 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          -- "__pycache__",
+          ".git",
+          ".vscode",
+          ".DS_Store",
+        },
+        never_show = {
+          ".php-cs-fixer.cache",
+        },
+        never_show_by_pattern = {
+          ".null-ls_*",
+        },
+      },
+      window = {
+        mappings = {
+          ["Y"] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+              filepath,
+              modify(filepath, ":."),
+              modify(filepath, ":~"),
+              filename,
+              modify(filename, ":r"),
+              modify(filename, ":e"),
+            }
+
+            vim.ui.select({
+              "1. Absolute path: " .. results[1],
+              "2. Path relative to CWD: " .. results[2],
+              "3. Path relative to HOME: " .. results[3],
+              "4. Filename: " .. results[4],
+              "5. Filename without extension: " .. results[5],
+              "6. Extension of the filename: " .. results[6],
+            }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+              local i = tonumber(choice:sub(1, 1))
+              local result = results[i]
+              vim.fn.setreg("+", result)
+              vim.notify("Copied: " .. result)
+            end)
+          end,
+        },
+      },
+    },
+  },
+}
