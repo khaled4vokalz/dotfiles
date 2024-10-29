e6311ca61abcf65911a1d0700b3f24b5d932e289
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2024-10-29 17:15:54 +0600

    use workspace feature of wezterm
    - it's (almost) the same as Tmuxs' sessions

--- wezterm/03-keys.lua
+++ wezterm/03-keys.lua
@@ -1,6 +1,9 @@
 local wezterm = require("wezterm")
 
 local keys = {
+	--[[
+  -- --------------------------------- SPLIT PANES ------------------------------------
+  --]]
 	{
 		key = "v",
 		mods = "LEADER",
@@ -11,6 +14,10 @@ local keys = {
 		mods = "LEADER",
 		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
 	},
+
+	--[[
+  -- --------------------------------- NAVIGATION ------------------------------------
+  --]]
 	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
 	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
 	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
@@ -21,17 +28,84 @@ local keys = {
 	{ key = "K", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Up", 2 } }) },
 	{ key = "L", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Right", 2 } }) },
 
+	-- Open new tab using current panes' PWD (domain)
 	{ key = "c", mods = "LEADER|CTRL", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+
+	-- Close current pane
 	{ key = "c", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 
-	-- zoom pane
+	--[[
+  -- --------------------------------- COPY ----------------------------------------
+  --]]
+	{ key = "Space", mods = "CTRL|SHIFT", action = "ActivateCopyMode" },
+
+	--[[
+  -- -------------------------------- ZOOM -----------------------------------------
+  --]]
 	{
 		key = "z",
 		mods = "LEADER",
 		action = wezterm.action.TogglePaneZoomState,
 	},
 
-	{ key = "Space", mods = "CTRL|SHIFT", action = "ActivateCopyMode" },
+	--[[
+  -- ------------------------------ WORKSPACES -------------------------------------
+  --]]
+	-- list tabs in a workspace
+	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|TABS" }) },
+
+	-- list workspaces
+	{ key = "l", mods = "SHIFT|ALT", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+
+	-- kill a whole workspace forcefully
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action_callback(function(win, pane)
+			local tab_ids = win:tabs_with_info() -- gets all tabs in the current window
+			for _, tab_info in ipairs(tab_ids) do
+				win:close_tab(tab_info.tab_id) -- closes each tab by its ID
+			end
+		end),
+	},
+
+	-- monitoring workspace
+	{
+		key = "u",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SwitchToWorkspace({
+			name = "monitoring",
+			spawn = {
+				args = { "htop" },
+			},
+		}),
+	},
+
+	-- Prompt for a name to use for a new workspace and switch to it.
+	{
+		key = "w",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						wezterm.action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
 }
 
 for i = 1, 9 do
