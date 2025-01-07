1cbdeb77ba1d0c50f96d808d20af25a27942a755
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2025-01-07 08:31:08 +0600

    fix adjust monitors
    - the previous implementation would return the laptop screen as well
    which we don't want to scale.
    - as the swaymsg get_outputs return JSON data, it's much easier to filter out things using `jq`

--- sway/scripts/adjust_monitors.sh
+++ sway/scripts/adjust_monitors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 scale_monitors() {
-  for monitor in $(swaymsg -t get_outputs -r | grep DP | cut -f2 -d":" | tr -d \",); do
-    swaymsg output $monitor scale 2
+  for monitor in $(swaymsg -t get_outputs -r | jq -r '.[] | select(.name | startswith("DP")) | .name'); do
+    swaymsg output "$monitor" scale 2
   done
 }
 
