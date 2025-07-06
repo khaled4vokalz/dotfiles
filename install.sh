57eb21e14cdda7462f29b5d71447770b47bbb7aa
Md. Mohiuddin Khaled Maruf <maruf.khaled@gmail.com>
2025-07-06 12:18:12 +0600

    Use the one liner installation of brave browser
    - makes life easier

--- install.sh
+++ install.sh
@@ -104,10 +104,7 @@ setup_starship() {
 
 ### Brave browser
 setup_brave_browser() {
-  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-  sudo apt update
-  sudo apt install -y brave-browser
+  curl -fsS https://dl.brave.com/install.sh | sh
 }
 
 ### VSCode
